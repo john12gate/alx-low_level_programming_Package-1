b4dc68de8af93f0d5b13028f457e716991de76ea
John Ali <alionuche2008@gmail.com>
2022-12-11 03:13:42 -0800

    Random Number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,35 +22,22 @@
  *
  * */
 
-int main (void)
-
-{
+int main (void){
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	/* Code to check for diffeent values of n */
-	
-
 	if (n == 0)
 	{
 		printf ("%i is zero \n", n);
-
 	}
-
 	else if (n < 0)
 	{
 		printf ("%i is negative\n", n);
-
 	}
-
 	else 
 	{
 		printf ("%i is positive\n", n);
-
 	}
-
 	return (0);
-
-
 }
