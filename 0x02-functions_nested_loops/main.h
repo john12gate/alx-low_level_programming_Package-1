0c1b1aba1599154bf11948944d24c2758fe654c7
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2022-12-14 11:40:07 -0800

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c); /** A function that prints _putchar */
-void print_alphabet(char a); /** A function that prints alphabets in small letters */
+int _putchar(char c);
+void print_alphabet(char a);
 
 #endif
