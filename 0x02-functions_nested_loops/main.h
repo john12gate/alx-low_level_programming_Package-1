f4553a914e0b74b21e3a449817c1726f06fdfddf
John Ali <alionuche2008@gmail.com>
2022-12-14 12:37:08 -0800

    Function File

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,6 @@
 i
 int _putchar(char c); /** A function to print _putchar */
 void print_alphabet(void); /** A function to print alphabet a - z */
-void print_alphabet_x10(void);/** A function to a - z in ten lines */
+void print_alphabet_x10(void); /** A function to a - z in ten lines */
 
 #endif
