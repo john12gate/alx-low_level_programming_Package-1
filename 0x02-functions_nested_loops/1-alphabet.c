97aa11a9d78c3eb523921a28486edaa5345260e2
John Ali <alionuche2008@gmail.com>
2022-12-14 10:51:06 -0800

    Print a - z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints small letters a - z
+ * @c - char variables for holding alphabets
+ * Author - John Ali
+ * Email - alionuche2008@gmail.com
+ *
+ * Description - A function that prints a - z
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _putchar(char c); /** A function that prints _putchar */
+
+int print_alphabet(char a); /** A function that prints alphabets in small letters */
 
 #endif
