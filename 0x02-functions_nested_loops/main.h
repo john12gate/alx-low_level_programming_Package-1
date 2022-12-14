6f72dc0ef6c7ecaddb44ddc07b579ed933779ad6
John Ali <alionuche2008@gmail.com>
2022-12-14 14:12:57 -0800

    Detect alphabet aA - zZ

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _putchar(char c); /** A function to print _putchar */
 void print_alphabet(void); /** A function to print alphabet a - z */
 void print_alphabet_x10(void); /** A function to a - z in ten lines */
 int _islower(int c); /** Function to detect if a letter is not lowercase */
+int _isalpha(int c); /** A function that checks for alphabetic character */
 
 #endif
