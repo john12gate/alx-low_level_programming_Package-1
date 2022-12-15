5047a2ec005626546788297a000f5918c0171305
John Ali <alionuche2008@gmail.com>
2022-12-14 16:40:42 -0800

    Function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _putchar(char c); /** A function to print _putchar */
 void print_alphabet(void); /** A function to print alphabet a - z */
 void print_alphabet_x10(void); /** A function to a - z in ten lines */
 int _islower(int c); /** Function to detect if a letter is not lowercase */
-int _isalpha(int c); /** A function that checks for alphabetic character */
+int _isalpha(int v); /** A function that checks for alphabetic character */
 int print_sign(int n); /** A function that prints the sign of a number */
 int _abs(int); /** A function that prints absolute values of a number */
 int print_last_digit(int); /** A function that returns the last digit of a number */
