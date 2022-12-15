19503a5e9207f9e4fa27e76012d75d4f593aaee1
John Ali <alionuche2008@gmail.com>
2022-12-14 16:30:40 -0800

    Jack Bauer Time

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,6 @@ int _isalpha(int c); /** A function that checks for alphabetic character */
 int print_sign(int n); /** A function that prints the sign of a number */
 int _abs(int); /** A function that prints absolute values of a number */
 int print_last_digit(int); /** A function that returns the last digit of a number */
+void jack_bauer(void); /** A function that prints time for jack bauer */
 
 #endif
